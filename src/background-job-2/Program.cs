2248518cc64f2df5fc647e72c988adcfac00eb5c
Ogbosuka Christopher <ogbosukachris@gmail.com>
2023-01-21 06:07:55 +0100

    upate

---
+++ src/background-job-2/Program.cs
@@ -0,0 +1,11 @@
+﻿using background_job_2;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
+
