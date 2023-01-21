68038013039e22f29297cbb8d08561f34dd6397f
Ogbosuka Christopher <ogbosukachris@gmail.com>
2023-01-21 05:17:42 +0100

    init

---
+++ src/git-action-back-ground-proj/Program.cs
@@ -0,0 +1,11 @@
+﻿using git_action_back_ground_proj;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
+
