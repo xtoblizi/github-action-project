a3b5b8285a4639668773af1b080e9edaf75e7073
Ogbosuka Christopher <ogbosukachris@gmail.com>
2023-01-22 23:07:13 +0100

    Main method

--- src/git-actions-project/Program.cs
+++ src/git-actions-project/Program.cs
@@ -3,7 +3,7 @@ namespace git_actions_project
 {
 	public class Program
 	{
-		public static void main(string[] args)
+		public static void Main(string[] args)
 		{
 			Console.WriteLine("App is starting");
 		}
